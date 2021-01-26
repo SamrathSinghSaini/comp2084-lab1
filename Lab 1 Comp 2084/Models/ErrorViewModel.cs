62eaf9e4ef8045acbac19c439b4608c54d459cc3
Samrath Singh Saini <200446420@student.georgianc.on.ca>
2021-01-26 13:55:21 -0500

    Add project files.

---
+++ Lab 1 Comp 2084/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_1_Comp_2084.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
